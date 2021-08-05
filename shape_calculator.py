369b8bc371fd97443f1526af88c2e57ee69ee24d
LeisAlAyoubi <>
2021-08-05 11:32:22 +0000

    Implemented Square Class and debug Rectangle Class

--- shape_calculator.py
+++ shape_calculator.py
@@ -25,12 +25,13 @@ class Rectangle:
     '''
     Returns a string that represents the shape using lines of "*". The number of lines should be equal to the height and the number of "*" in each line should be equal to the width. There should be a new line (\n) at the end of each line. If the width or height is larger than 50, this should return the string: "Too big for picture.".
     '''
+    text = ""
     if self.height > 50 or self.width > 50:
       return "Too big for picture."
     else:
-      for i in range(len(self.height)):
-        print("*" * self.width + "\n")
-
+      for i in range(self.height):
+        text += ("*" * self.width + "\n")
+      return text
 
   def get_amount_inside(self, shape): 
     '''
@@ -44,6 +45,20 @@ class Rectangle:
     else:
       return height_fit * width_fit
 
-class Square():
-  pass
+class Square(Rectangle):
+  def __init__(self,side_length):
+    Rectangle.height = side_length
+    Rectangle.width = side_length
 
+  def __repr__(self):
+      return "Square(side=" + str(Rectangle.height)+")"
+
+  def set_width(self, side):
+    Rectangle.width = side
+
+  def set_height(self, side):
+    Rectangle.height = side
+
+  def set_side(self, side):
+    Rectangle.width = side
+    Rectangle.height = side
